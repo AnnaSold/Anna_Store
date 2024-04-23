888682d7c9fa456e80e28a559d5675f27381eec8
Anna <anutka9602@mail.ru>
2024-04-23 22:00:54 +0300

    list

--- src/main/java/Anna/ProductService.java
+++ src/main/java/Anna/ProductService.java
@@ -7,10 +7,19 @@ import java.util.ArrayList;
 import java.util.List;
 @Component
 public class ProductService {
-    @Autowired
-    private OrderService orderService;
 
-    List<Product> catalog;
+    private List<Product> catalog;
+
+    public ProductService() {
+        catalog.add(new Product(1,"Strawberrry", 300.0));
+        catalog.add(new Product(2,"chicken fillet", 429.0));
+        catalog.add(new Product(3,"Beer", 149.50));
+        catalog.add(new Product(4,"spices paprika", 79.90));
+        catalog.add(new Product(5,"lemon", 49.90));
+        catalog.add(new Product(6,"B.U. Alexandrov", 59.90));
+        catalog.add(new Product(7,"Milk", 94.0));
+        catalog.add(new Product(8,"Cereal", 199.0));
+    }
 
     public void fillList(List<Product> products) {
         for (Product c : catalog) {
@@ -18,7 +27,7 @@ public class ProductService {
         }
 
 
-        public static void getProductInConsol() {
+        public static void getProductInConsol(List <Product> catalog) {
 
             for (Product c : catalog) {
                 System.out.println(c);
