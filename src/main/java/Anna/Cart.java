f61f087be950b29dba8458629c11d036f99c6299
Anna Sold <anutka9602@mail.ru>
2024-04-24 10:53:10 +0300

    orderService

--- src/main/java/Anna/Cart.java
+++ src/main/java/Anna/Cart.java
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Component
 public class Cart {
     List<Product> cart = new ArrayList<>();
-    public  void addProduct(Product p){
-        cart.add(p);
+
+    public void addProduct(Product p) {
+        if (p != null)
+            cart.add(p);
     }
 }
